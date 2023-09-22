7bdc024865af576c6138e1ae81d71f5985bb92c7
Ravi Avula <raviavula.gcp@gmail.com>
2023-09-22 11:42:41 +0100

    Storage class change

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "tf_demo" {
     name = "terraform_demo_gcp_bucket_202322"
     location = "europe-west2"
-    storage_class = "standard"
+    storage_class = "nearline"
   }
